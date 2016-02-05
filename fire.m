cfecd2361468d4e72dbfd2457d532261edf50615
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2016-02-05 13:04:17 +0000

    remove tempScale from fire.m

--- fire.m
+++ fire.m
@@ -176,19 +176,4 @@ function [img alpha] = ...
   endfor
 endfunction
 
-function cMap = tempScale()
-
-  %    grey black red red    orange whitish yellow red
-  r = [0.5  0     1   1      1      1       1      1   ];
-  g = [0.5  0     0   0      0.5    .9      1      0   ];
-  b = [0.5  0     0   0      0      .8      0      0   ];
-  a = [0    1     1   1      1      1       1      1  ]*.1;
-  i = [1    15    20  30     50     80      95     100 ];
-  
-  cMap = zeros( 100, 4); % RGBA
-  cMap( : , 1 ) = interp1( i, r, 1:100 );
-  cMap( : , 2 ) = interp1( i, g, 1:100 );
-  cMap( : , 3 ) = interp1( i, b, 1:100 );
-  cMap( : , 4 ) = interp1( i, a, 1:100 );
-endfunction
 

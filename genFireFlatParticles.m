3dd8450614ca31834b6f2767d0e2b14aad2d9628
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2016-02-05 21:19:00 +0000

    split particle creation and page setting into new scripts

---
+++ genFireFlatParticles.m
@@ -0,0 +1,9 @@
+setPage
+particleOrigin = [128+border,220+border]
+nStreamers = 2
+particles = initFireStreamers(nStreamers, particleOrigin);
+particleFiles=[];
+for i=1:nTime
+    particleFile = sprintf('fire_flat_particles_%02i.dat',i)
+    save(particleFile,'particles'); % save same data to start each time step.
+endfor
---
+++ setPage.m
@@ -0,0 +1,3 @@
+border = 32
+imgSize = 256+2*border
+whiteBg = 0  % 1:not transparent

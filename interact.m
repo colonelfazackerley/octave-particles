c34953dbba03042a6563bf80f51235c53e5a8cec
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2016-02-10 21:16:16 +0000

    interact faster for lots of particles. break even point compared to previous fn 6-8 particles

--- interact.m
+++ interact.m
@@ -1,24 +1,60 @@
 % change velocities modelling forces between particles
 function particles = interact( particles, interactionParams )
-  for i=1:length(particles)
-    for j=1:length(particles)
-      if j>=i
-        continue
-      endif
-      p_i = particles(i);
-      p_j = particles(j);
-      if interactionParams.fExclusionZone(p_i.pos) && interactionParams.fExclusionZone(p_j.pos)
-        continue
-      endif
-      d = p_i.pos - p_j.pos; % displacement vector
-      distsq = sum(d.^2);
-      if distsq > interactionParams.distSqLimit
-        continue
-      endif
-      f = interactionParams.fForce(distsq,d);
-      particles(i).vel += f ;
-      particles(j).vel += -f ;
+    poss = vertcat(particles.pos);
+
+    xsi = poss(:,1);
+    xsj = xsi';
+    [Xsi Xsj]=meshgrid(xsi, xsj);
+    
+    ysi = poss(:,2);
+    ysj = ysi';
+    [Ysi Ysj]=meshgrid(ysi, ysj);
+    
+    dX = Xsi - Xsj;
+    dY = Ysi - Ysj;
+    dSq= dX.^2 + dY.^2;
+    
+    fX = interactionParams.fForce( dSq, dX );
+    fY = interactionParams.fForce( dSq, dY );
+    
+    diagMask = logical(diag(ones(1,length(particles))));
+    
+    fX(diagMask)=0; %particles dont interact with themselves
+    fY(diagMask)=0;
+    
+    fX(dSq>interactionParams.distSqLimit) = 0;
+    fY(dSq>interactionParams.distSqLimit) = 0;
+    
+    sumXf = sum(fX); % all forces on each particle
+    sumYf = sum(fY);
+    
+    for i=1:length(particles)
+        particles(i).vel += [sumXf(i) sumYf(i)];
     endfor
-  endfor
+
 endfunction
 
+%!demo
+%!  for i=1:8
+%!      p.pos=rand(1,2)*10;
+%!      p.vel=[0 0];
+%!      particles(i)=p;
+%!  end
+%!  #Inverse square law
+%!  iParams.fForce = @(distSq, displacement) -0.01 * displacement ./ (distSq.^1.5);
+%!  iParams.distSqLimit = 100;
+%!  iParams.fExclusionZone = @(pos) 0;
+%!  totTime=0;
+%!  for i=1:1000
+%!      coords = vertcat(particles.pos);
+%!      plot(coords(:,1),coords(:,2),'x')
+%!      axis([-3 3 -3 3],"equal")
+%!      %print(sprintf("interact_demo_%03i.png",i))
+%!      tic;
+%!      particles=interact(particles, iParams);
+%!      totTime += toc;
+%!      for j=1:length(particles)
+%!          particles(j).pos += particles(j).vel;
+%!      endfor
+%!  endfor
+%!  totTime

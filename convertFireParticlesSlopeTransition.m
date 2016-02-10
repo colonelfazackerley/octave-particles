35e9c4fefc647f3e11c483b83fc75410883aed8f
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2016-02-10 21:18:11 +0000

    make transitional animation for falling on to slopes

---
+++ convertFireParticlesSlopeTransition.m
@@ -0,0 +1,39 @@
+function convertFireParticlesSlopeTransition( variant )
+    fallDatGlob = sprintf('fire_falling_particles_%s_*.dat', variant);
+    rightDatGlob = sprintf('fire_rise_right_particles_%s_*.dat',variant );
+    leftDatGlob = sprintf('fire_rise_left_particles_%s_*.dat',variant );
+    fallFiles = dir(fallDatGlob);
+    rightFiles = dir(rightDatGlob);
+    leftFiles = dir(leftDatGlob);
+    setPage
+    nTime = length(fallFiles);
+    
+    transSpeed = 25 ; % speed to move from one position to another. pixels per time step
+    
+    for t=1:nTime
+        % right rise
+        load( fallFiles(t).name, 'particles' );
+        fallPs = particles;
+        load( rightFiles(t).name, 'particles' );
+        slopePs = particles;
+        nParticles = length(particles);
+        for i=1:nParticles
+            fpos = fallPs(i).pos + [ 0 128]; % transform onto larger page [x y]
+            spos = slopePs(i).pos;
+            dirn = spos-fpos ;
+            dist = sqrt(sum(dirn.^2));
+            unitv = dirn/dist;
+            transDist = transSpeed * (t-1);
+            if transDist > dist
+                particles(i).pos = spos; % stop when arrived at final position
+            else
+                particles(i).pos = fpos + unitv * transDist;
+            endif
+        endfor
+        particleFile = sprintf('fire_fall_to_rise_right_particles_%s_%02i.dat',variant, t)
+        save(particleFile,'particles');
+        % left rise
+        
+        
+    endfor
+endfunction
---
+++ runFireFallToRiseRight.m
@@ -0,0 +1,29 @@
+function runFireRiseRight(variant)
+    % config
+    nTime = 10 ; % number of time steps
+    nSteps = 52 %100; % streamer length steps
+    cMap = tempScale();
+    maxWidth = 10 ; % max half width of streamers
+    setPage
+
+    vertOffset = 128;
+    
+    imgSize(1) = imgSize(1)+vertOffset; %imgSize is height, width
+
+    pngPrefix = sprintf("fire_fall_to_rise_right_%s_",variant);
+
+    interactionParams.fExclusionZone = @(pos) pos(2) >= 200 + 32 + 128 - pos(1);
+    interactionParams.distSqLimit = 3000;
+    interactionParams.fForce = @(distSquared, displacement) 0.4 * displacement / distSquared; 
+
+    particleParams.fCooling = @(pos) (265 + 32 + 128 + 40 -pos(1) - pos(2))/120;
+
+    testPlot(imgSize, border, interactionParams, particleParams);
+    datGlob = sprintf('fire_fall_to_rise_right_particles_%s_*.dat',variant);
+    fire(datGlob, nSteps, cMap, imgSize, maxWidth, ...
+         particleParams, interactionParams, pngPrefix, whiteBg);
+
+    cmd = sprintf("convert -loop 0 -delay 10 o_%s*.png  %s.gif",pngPrefix,pngPrefix);
+    printf("%s\n",cmd);
+    system(cmd);
+endfunction
